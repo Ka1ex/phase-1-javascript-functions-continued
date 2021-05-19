e4e94abb0c7c91345497bf16c9c810fdbd320550
ka1ex <kalexadamson@hotmail.com>
2021-05-18 19:17:04 -0500

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(whatADay="roller-skate"){
+ return `This Saturday, I want to ${whatADay}!`
+}
+function mondayWork(onMonday="go to the office"){
+    return `This Monday, I will ${onMonday}.`
+}
+function wrapAdjective(maybe="*"){
+ return function(feelingGood){
+     return `You are ${maybe}${feelingGood}${maybe}!`
+ }
+}
